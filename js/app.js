b0250dd6e665e2e277a2ec50e8b7964d605e685a
Bogdan <buselb2@gmail.com>
2020-10-15 15:11:13 +0300

    create intro

---
+++ js/app.js
@@ -0,0 +1,11 @@
+VANTA.CELLS({
+ el: "#body",
+ mouseControls: true,
+ touchControls: true,
+ gyroControls: false,
+ minHeight: 200.00,
+ minWidth: 200.00,
+ scale: 1.00,
+ color1: 0x1e1e1e,
+ color2: 0x434328
+})
