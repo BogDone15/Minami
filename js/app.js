412c9ac2d115853968b9c4647f5fc7c229ff1841
Bogdan <buselb2@gmail.com>
2020-10-20 20:01:57 +0300

    add carousel content

--- js/app.js
+++ js/app.js
@@ -1,11 +1,21 @@
-VANTA.CELLS({
- el: "#body",
- mouseControls: true,
- touchControls: true,
- gyroControls: false,
- minHeight: 200.00,
- minWidth: 200.00,
- scale: 1.00,
- color1: 0x1f2a2a,
- color2: 0x141411
-})
+// VANTA.CELLS({
+//  el: "#body",
+//  mouseControls: true,
+//  touchControls: true,
+//  gyroControls: false,
+//  minHeight: 200.00,
+//  minWidth: 200.00,
+//  scale: 1.00,
+//  color1: 0x1f2a2a,
+//  color2: 0x141411
+// })
+
+$(document).ready(function(){
+ $(".owl-carousel").owlCarousel({
+  margin: 20,
+  items: 2,
+  nav: true,
+  dots: false,
+  navText: ["",""]
+ });
+});
